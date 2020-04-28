24a867a54e95ebcf67ed3a1032e9d25e2b51fec2
p1983kalbrun <53264036+p1983kalbrun@users.noreply.github.com>
2020-04-28 10:46:57 +0100

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/na/na/na/QwertyApplication.java
@@ -0,0 +1,12 @@
+package com.na.na.na;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwertyApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(QwertyApplication.class, args);
+	}
+
+}
